5623b46313d84250661cfd2dc3f0a291926db0e7
nankuF <fireloki87@gmail.com>
2022-06-27 21:58:41 +0600

    added autoposting in telegram channel

---
+++ auto_posting.py
@@ -0,0 +1,63 @@
+import argparse
+import random
+import sys
+import time
+from datetime import datetime
+
+import environs
+import telegram
+
+from utils import read_dirs
+
+env = environs.Env()
+env.read_env()
+token = env.str('TG_TOKEN')
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dir', required=True, type=str)
+    parser.add_argument('--hour', required=True, type=int, default=4)
+
+    return parser
+
+
+def auto_posting(dir_: str, interval: int):
+    """
+    Публикация фотографий из определенной директории с определенным интервалом в часах.
+    :param dir_: директория, откуда загружать фото. (пример: '/images/nasa_apod').
+    :param interval: интервал между публикациями фото, в часах.
+    """
+    chat_id = '@nasa_spacex_images_channel'
+
+    bot = telegram.Bot(token=token)
+
+    images = read_dirs(path=dir_)
+    random.shuffle(images)
+    now, update = datetime.now(), datetime.now()
+    n = 0
+    while True:
+        time.sleep(60)
+        now = datetime.now()
+        if now.hour - update.hour >= interval:
+            if len(images):
+                bot.send_photo(chat_id=chat_id, photo=open(images[n], 'rb'))
+                print('Фото опубликовано')
+                update = datetime.now()
+                if n >= len(images):
+                    n = 0
+                    random.shuffle(images)
+                else:
+                    n += 1
+            else:
+                print('Нет фотографий.')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        auto_posting('images/nasa_apod', interval=1)
+    else:
+        parser = create_parser()
+        namespace = parser.parse_args()
+        auto_posting(dir_=namespace.dir, interval=namespace.hour)
+
